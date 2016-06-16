d7924640fe8a7532751737cda27dcec0223acc17
Sudio Tan <sudio_tan@trend.com.tw>
2016-06-16 18:27:27 +0800

    Template parameters externalized to config.json

---
+++ DeepSecurityAddExtension_Win.ps1
@@ -0,0 +1,157 @@
+﻿ [CmdletBinding()]
+ param (
+    #[Parameter(Mandatory=$true)][string]$protectedFileName,
+    #[Parameter(Mandatory=$true)][string]$publicFileName,
+    #[Parameter(Mandatory=$true)][string]$vmListFileName
+    
+    #TODO change to Mandatory
+    [string]$protectedFileName = "private.config",
+    [string]$publicFileName = "public.config",
+    [string]$vmListFileName = "VMList.config"
+ )
+
+if([string]::IsNullOrWhiteSpace($protectedFileName) -or [string]::IsNullOrWhiteSpace($publicFileName) -or [string]::IsNullOrWhiteSpace($vmListFileName)) {
+    throw "Please provide mandatory parameters"
+}
+
+try {
+    $protectedFileContent = Get-Content -Raw -LiteralPath $protectedFileName -Encoding UTF8 -ErrorAction Stop
+    $publicFileContent = Get-Content -Raw -LiteralPath $publicFileName -Encoding UTF8 -ErrorAction Stop
+    $vmListFileContent = Get-Content -Raw -LiteralPath $vmListFileName -Encoding UTF8 -ErrorAction Stop
+} catch {
+    throw "Error while reading configuration files : " + $_
+}
+
+try {
+    [void] (ConvertFrom-JSON -InputObject $protectedFileContent -ErrorAction Stop)
+    [void] (ConvertFrom-JSON -InputObject $publicFileContent -ErrorAction Stop)
+    $vmList = ConvertFrom-JSON -InputObject $vmListFileContent -ErrorAction Stop
+
+    # if not array, wrap in array
+    if($vmList -isnot [array]) { $vmList = @($vmList) }
+} catch {
+    throw "Configuration files not a valid JSON format : " + $_
+}
+
+<#
+$location = "East US"
+$AzureAccount = "twdsauto@dsauto.onmicrosoft.com"
+$AzurePassword = '2010@Azure!@#'
+$securePassword = ConvertTo-SecureString -AsPlainText -Force $AzurePassword
+$AzureSubscriptionID = "dabc1645-8ed8-4f30-a200-0c1c55b33657"
+
+echo ("{0} Login into Azure" -f $(Get-Date ).ToString())
+$credential = New-Object System.Management.Automation.PSCredential $AzureAccount, $securePassword
+Login-AzureRmAccount -Credential $credential
+Select-AzureRmSubscription -SubscriptionId $AzureSubscriptionID
+#>
+
+
+#region List all available extensions version by location
+echo ("{0} List all available extensions" -f $(Get-Date ).ToString())
+
+$TMPublisher = "TrendMicro.DeepSecurity"
+$TMextensionname = "TrendMicroDSA"
+$dsaExtLocationVersionTable = @{}
+$vmList | Select-Object -Property location -Unique | foreach {
+    try {
+        $dsaExtVersion = "9.6"  # default extension version
+        $dsaExtVersionImages = Get-AzureRmVMExtensionImage -Location $_.location –PublisherName $TMPublisher -Type $TMextensionname -ErrorAction Stop
+        
+        $ver = $dsaExtVersion
+
+        if($dsaExtVersionImages -is [Object[]]) {
+            $ver = $dsaExtVersionImages[$dsaExtVersionImages.Length-1].Version
+        }elseif($dsaExtVersionImages -is [Object]) {
+            $ver = $dsaExtVersionImages.Version
+        }
+        if( $ver -match "(\d+\.\d+)\.*") {  # get first two version-number (eg : get A.B from A.B.C.D)
+            $dsaExtVersion = $Matches[1]
+        }
+
+        $dsaExtLocationVersionTable.($_.location) = $dsaExtVersion
+
+    }catch {
+        throw "Error while query for all available extensions : " + $_
+    }    
+}
+
+# show versions
+$dsaExtLocationVersionTable | Format-Table
+
+#endregion 
+
+
+#region Add extensions for every VMs
+
+$minThreadCount = 1
+$maxThreadCount = 100
+$SessionState = [System.Management.Automation.Runspaces.InitialSessionState]::CreateDefault()
+$RunspacePool = [runspacefactory]::CreateRunspacePool($minThreadCount, $maxThreadCount)
+$RunspacePool.Open()
+$jobs = New-Object System.Collections.ArrayList
+$scriptBlock = {
+    Param (
+        $resourceGroupName,
+        $location,
+        $vmName,
+        $name,
+        $publisher,
+        $extensionType,
+        $typeHandlerVersion,
+        $settingString,
+        $protectedSettingString
+    )
+            
+    $ThreadID = [appdomain]::GetCurrentThreadId()
+      
+    Set-AzureRMVMExtension -ResourceGroupName $resourceGroupName -Location $location -VMName $vmName -Name $name -Publisher $publisher -ExtensionType $extensionType -TypeHandlerVersion $typeHandlerVersion -SettingString $settingString -ProtectedSettingString $protectedSettingString
+    echo ("{0} Thread:{1} Finished adding extension for VM:{2}; ResourceGroup:{3}; Extension:{4}; Version:{5}; Location:{6}" -f $(Get-Date ).ToString(), $ThreadID, $vmName, $resourceGroupName, $name, $typeHandlerVersion, $location)
+}
+
+echo ("{0} Adding VM Extension." -f $(Get-Date ).ToString())
+
+#for($i = 0; $i -lt $numberOfVM ; $i++){
+
+$vmList | ForEach {
+    $VMName = $_.vm_name
+    $resourceGroupName = $_.resource_group
+    $location = $_.location
+    $version = $dsaExtLocationVersionTable[$location]
+    $Parameters = @{
+                    resourceGroupName=$resourceGroupName
+                    location=$location
+                    vmName=$VMName
+                    name=$TMextensionname
+                    publisher=$TMPublisher
+                    extensionType=$TMextensionname
+                    typeHandlerVersion=$version
+                    settingString=$publicFileContent
+                    protectedSettingString=$protectedFileContent
+                    }
+        
+    $PowerShell = [powershell]::Create() 
+    $PowerShell.RunspacePool = $RunspacePool
+        
+    echo ("{0} Adding extension for VM:{1}; ResourceGroup:{2}; Location:{3}; Version:{4}" -f $(Get-Date ).ToString(), $VMName, $_.resource_group, $location, $version)
+
+    [void]$PowerShell.AddScript($scriptBlock)
+    [void]$PowerShell.AddParameters($Parameters)
+    $Handle = $PowerShell.BeginInvoke()
+
+    $temp = "" | Select PowerShell,Handle
+    $temp.PowerShell = $PowerShell
+    $temp.handle = $Handle
+    [void]$jobs.Add($temp)
+
+    sleep 2
+}
+    
+$jobs | ForEach {
+    $_.powershell.EndInvoke($_.handle)
+    $_.PowerShell.Dispose()
+}
+
+$jobs.clear()
+
+#endregion 
---
+++ DeepSecurityAddVM.ps1
@@ -0,0 +1,84 @@
+﻿ [CmdletBinding()]
+ param (
+    #[Parameter(Mandatory=$true)][string]$configFile,
+    
+    #TODO change to Mandatory
+    [string]$configFileName = "config.json"
+ )
+
+try {
+    $configFile = Get-Content -Raw -LiteralPath $configFileName -Encoding UTF8 -ErrorAction Stop
+} catch {
+    throw "Error while reading configuration files : " + $_
+}
+
+try {
+    $configurations = ConvertFrom-JSON -InputObject $configFile -ErrorAction Stop
+} catch {
+    throw "Configuration file not a valid JSON format : " + $_
+}
+
+
+if([string]::IsNullOrWhiteSpace($configurations.location) `
+    -or [string]::IsNullOrWhiteSpace($configurations.numberOfVM) `
+    -or [string]::IsNullOrWhiteSpace($configurations.resourceNamePrefix) `
+    -or [string]::IsNullOrWhiteSpace($configurations.azureAccount) `
+    -or [string]::IsNullOrWhiteSpace($configurations.azurePassword) `
+    -or [string]::IsNullOrWhiteSpace($configurations.azureSubscriptionID)) {
+    throw "Please provide mandatory parameters"
+}
+
+#region Login into Azure
+
+$securePassword = ConvertTo-SecureString -AsPlainText -Force $configurations.azurePassword
+$AzureSubscriptionID = "dabc1645-8ed8-4f30-a200-0c1c55b33657"
+
+echo ("{0} Login into Azure" -f $(Get-Date ).ToString())
+$credential = New-Object System.Management.Automation.PSCredential $configurations.azureAccount, $securePassword
+Login-AzureRmAccount -Credential $credential
+Select-AzureRmSubscription -SubscriptionId $configurations.azureSubscriptionID
+
+#endregion
+
+
+#region Create VM Using Deployment Template
+
+# vm information
+$numberOfVM = $configurations.numberOfVM
+$resourceNamePrefix = $configurations.resourceNamePrefix
+$resourceGroupName = $resourceNamePrefix + "rscgrp"
+$VMNamePrefix = $resourceNamePrefix + "arm"
+$deployName = $resourceNamePrefix + "deploy" + ([guid]::NewGuid().ToString()).substring(0,4)
+$templateURI = $configurations.deploymentTemplateURI
+
+$params = @{
+    vmName = $VMNamePrefix
+    numberOfVM = $numberOfVM
+    vmAdminUsername = $configurations.vmAdminUsername
+    vmAdminPassword = $configurations.vmAdminPassword
+    vmSize = $configurations.vmSize
+    imagePublisher = $configurations.imagePublisher
+    imageOffer = $configurations.imageOffer
+    imageSku = $configurations.imageSku
+    imageVersion = $configurations.imageVersion
+}
+
+echo ("{0} Creating resource group : {1}" -f $(Get-Date ).ToString(), $resourceGroupName)
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $configurations.location
+
+echo ("{0} Deploying {1} resources using ResourceGroup:{2}; deployName:{3}; template:{4};" -f $(Get-Date ).ToString(), $numberOfVM, $resourceGroupName, $deployName, $templateURI)
+New-AzureRmResourceGroupDeployment -Name $deployName -ResourceGroupName $resourceGroupName -TemplateUri $templateURI -TemplateParameterObject $params
+echo ("{0} Finished deploying template:{1}" -f $(Get-Date ).ToString(), $templateURI)
+
+#endregion
+
+
+#region Dump created VMs to JSON file
+
+$vmJsonArr = New-Object System.Collections.ArrayList
+0..($numberOfVM - 1) | foreach {
+    $vmJsonArr.add(@{resource_group =$resourceGroupName; vm_name=$VMNamePrefix + $_ ;location=$configurations.location})
+}
+$vmJsonArr |ConvertTo-Json |Out-File -Encoding utf8 -Force ($PSScriptRoot + "\" + "VMList.config")
+
+#endregion
